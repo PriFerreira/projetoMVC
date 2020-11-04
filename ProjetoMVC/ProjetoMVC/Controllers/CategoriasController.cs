78d288444746e1ab4f3f5b2a8bed5bb8adda08c5
Priscila dos Santos Ferreira Lacerda <priscilaf@partnersistemas.com.br>
2020-11-04 09:58:41 -0300

    Iniciando projeto.

---
+++ ProjetoMVC/ProjetoMVC/Controllers/CategoriasController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria()
+            {
+                CategoriaID = 1,
+                Nome = "Notebooks"
+            },
+            new Categoria()
+            {
+                CategoriaID = 2,
+                Nome = "Monitores"
+            },
+            new Categoria()
+            {
+                CategoriaID = 3,
+                Nome = "Impressoras"
+            },
+            new Categoria
+            {
+                CategoriaID = 4,
+                Nome = "Mouses"
+            },
+            new Categoria()
+            {
+                CategoriaID = 5,
+                Nome = "Desktops"
+            }
+        };
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);//poderia ser uma ViewResult ao inves de ActionResult devido o retorno ser de uma View
+        }
+
+    }
+}
