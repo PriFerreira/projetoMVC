134db02c524cd11b40d565c843bd74421f229707
Priscila dos Santos Ferreira Lacerda <priscilaf@partnersistemas.com.br>
2020-11-06 08:44:15 -0300

    Conclusão do capitulo 1.

--- ProjetoMVC/ProjetoMVC/Controllers/CategoriasController.cs
+++ ProjetoMVC/ProjetoMVC/Controllers/CategoriasController.cs
@@ -41,14 +41,18 @@ namespace ProjetoMVC.Controllers
         // GET: Categorias
         public ActionResult Index()
         {
-            return View(categorias);//poderia ser uma ViewResult ao inves de ActionResult devido o retorno ser de uma View
+            return View(categorias.OrderBy(
+                c => c.Nome));
+            /* OBS.: 0poderia ser uma ViewResult ao inves de 
+             * ActionResult devido o retorno ser de uma View*/
         }
 
         //	GET:	Create
         public ActionResult Create()
         {
             return View();
-        }
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Categoria categoria)
@@ -57,5 +61,55 @@ namespace ProjetoMVC.Controllers
             categoria.CategoriaID = categorias.Select(m => m.CategoriaID).Max() + 1;
             return RedirectToAction("Index");
         }
+
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(
+                m => m.CategoriaID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(
+                c => c.CategoriaID == categoria.CategoriaID).First());
+
+            /* Uma maneira alternativa para alterar um item da lista, sem ter de remover e inseri-lo novamente, 
+             * é fazer uso da implementação. Aqui o	List é manipulado como um array	e, por meio	do método 
+             * IndexOf(), sua posição é	recuperada,	com	base na instrução LINQ 
+             * Where(c => c.CategoriaId == categoria.CategoriaId).First())
+             * veja o exemplo abaixo:
+             */
+            /*categorias[categorias.IndexOf(categorias.Where(
+                c => c.CategoriaID == categoria.CategoriaID).First())] = categoria;*/
+
+
+            categorias.Add(categoria);            
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(
+                m => m.CategoriaID == id).First());
+        }
+
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(
+                m => m.CategoriaID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(
+                c => c.CategoriaID== categoria.CategoriaID).First());
+
+            return RedirectToAction("Index");
+        }
     }
 }
