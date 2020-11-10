9592daf137a32d01185b1ba43e311d5ca70c87d9
Priscila dos Santos Ferreira Lacerda <priscilaf@partnersistemas.com.br>
2020-11-10 09:28:35 -0300

    Conclusão do capítulo 2.

--- ProjetoMVC/ProjetoMVC/Controllers/FabricantesController.cs
+++ ProjetoMVC/ProjetoMVC/Controllers/FabricantesController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProjetoMVC.Contexts;
+using ProjetoMVC.Models;
 
 namespace ProjetoMVC.Controllers
 {
@@ -17,5 +20,74 @@ namespace ProjetoMVC.Controllers
             return View(context.Fabricantes.OrderBy(
                 c => c.Nome));
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long? id)
+        {
+            if(id == null)
+            {
+                return new
+                    HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if(ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
+        }
+
+        public ActionResult Delete(long? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            if(fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Fabricante fabricante = context.Fabricantes.Find(id);
+            context.Fabricantes.Remove(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
